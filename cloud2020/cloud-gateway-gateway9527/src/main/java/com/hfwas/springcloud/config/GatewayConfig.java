ffd52cb990ac5cc6d9761b1b222e651961f1fc0a
HFwas <1558934404@qq.com>
2021-11-24 23:58:52 +0800

    "feat://网关"

---
+++ cloud2020/cloud-gateway-gateway9527/src/main/java/com/hfwas/springcloud/config/GatewayConfig.java
@@ -0,0 +1,24 @@
+package com.hfwas.springcloud.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Author HFwas
+ * @Date 2021/11/24 23:46
+ */
+@Configuration
+public class GatewayConfig {
+
+    @Bean
+    public RouteLocator routeLocator(RouteLocatorBuilder routeLocatorBuilder){
+        return routeLocatorBuilder.routes()
+                .route("path_route1", route ->
+                        route.path("/guonei")
+                                .uri("http://news.baidu.com/guonei"))
+                .build();
+    }
+
+}
