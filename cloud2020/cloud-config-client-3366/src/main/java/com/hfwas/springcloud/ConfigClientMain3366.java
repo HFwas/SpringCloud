a255b75a6f18dbcdbdd24493b24b409b300f9639
HFwas <1558934404@qq.com>
2021-11-26 23:43:33 +0800

    "feat://config"

---
+++ cloud2020/cloud-config-client-3366/src/main/java/com/hfwas/springcloud/ConfigClientMain3366.java
@@ -0,0 +1,17 @@
+package com.hfwas.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Author HFwas
+ * @Date 2021/11/26 1:03
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3366 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3366.class, args);
+    }
+}
