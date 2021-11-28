5b0f16dd69427f2827438925ed7de1cec1245dc9
HFwas <1558934404@qq.com>
2021-11-28 19:12:47 +0800

    "feat://nacos的服务生产者"

---
+++ cloud2020/cloudalibaba-provider-payment9001/src/main/java/com/hfwas/springcloud/PaymentMain9001.java
@@ -0,0 +1,17 @@
+package com.hfwas.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Author HFwas
+ * @Date 2021/11/28 19:10
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain9001.class, args);
+    }
+}
---
+++ cloud2020/cloudalibaba-provider-payment9002/src/main/java/com/hfwas/springcloud/controller/PaymentController.java
@@ -0,0 +1,23 @@
+package com.hfwas.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author HFwas
+ * @Date 2021/11/28 19:11
+ */
+@RestController
+public class PaymentController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @GetMapping("/payment/nacos/{id}")
+    public String getPayment(@PathVariable("id") Integer id) {
+        return "nacos register, serverport=" + serverPort + "\t id:" + id;
+    }
+
+}
