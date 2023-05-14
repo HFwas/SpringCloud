8021ce6af27930cfffa8b382fcf7e554d901fb33
houfei <1558934404@qq.com>
2023-05-14 21:04:53 +0800

    junit测试框架

--- gulimall/gulimall/gulimall-order/src/test/java/com/atguigu/gulimallorder/GulimallOrderApplicationTests.java
+++ gulimall/gulimall/gulimall-order/src/test/java/com/atguigu/gulimallorder/GulimallOrderApplicationTests.java
@@ -1,13 +1,9 @@
 package com.atguigu.gulimallorder;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class GulimallOrderApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
 
 }
